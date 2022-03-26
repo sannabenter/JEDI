365221709efab76dfbbb404229d82ad8de366679
Sanna Benter <bentersanna@gmail.com>
2022-03-26 13:42:45 +0100

    changed theme again.

--- documentation/conf.py
+++ documentation/conf.py
@@ -48,7 +48,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'guzzle_sphinx_theme'
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
